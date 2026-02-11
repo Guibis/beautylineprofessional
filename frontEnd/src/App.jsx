598318c46b6328f35b51177b342722b6c5bfac07
guibis <guilhermeplima5@gmail.com>
2026-02-11 14:33:18 -0300

    feat: Main components and global styling using Tailwind CSS and custom fonts.

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,10 +1,27 @@
-function App() {
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Pillars from './components/Pillars';
+import EducationSection from './components/EducationSection';
+import TechnologySection from './components/TechnologySection';
+import CosmeticsSection from './components/CosmeticsSection';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
+function App() {
   return (
-    <>
-      <h1>Ciao mondo!</h1>
-    </>
-  )
+    <div className="min-h-screen bg-stone-50 text-stone-800 font-body">
+      <Navbar />
+      <main>
+        <Hero />
+        <Pillars />
+        <EducationSection />
+        <TechnologySection />
+        <CosmeticsSection />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
