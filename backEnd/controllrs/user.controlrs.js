a53592bed5f62ef03fe93f73e0ea14a9c21967c6
Igor Rodrigues Machado <107290125+igorrm19@users.noreply.github.com>
2026-02-19 06:14:49 -0300

    Feature controllrs user (#45)
    * Feature controllrs user
    
    * Code review, suggested changes
    
    * Update user.controlrs.js
    
    * Update user.controlrs.js
    
    * Refactor duplicated email error handling
    
    * Refactor error handling for user fetching
    
    * Fix duplicated email error handling condition
    
    * Handle CastError and duplicated email error responses
    
    * Refactor error handling and constant declaration
    
    * Fix error handling in user controller
    
    * Apply suggestion from @robdll
    
    ---------
    
    Co-authored-by: Roberto Di Lillo <dilillo.roberto@gmail.com>

---
+++ backEnd/controllrs/user.controlrs.js
@@ -0,0 +1,171 @@
+const User = require("../model/userDB.model");
+const bcrypt = require("bcrypt");
+
+const DUPLICATED_EMAIL_CODE = 11000;
+
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find().select('-password');
+
+        if (users.length === 0) {
+            return res.status(200).json([]);
+        }
+
+        res.status(200).json(users);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Error fetching users" });
+    }
+}
+
+
+const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).select('-password');
+
+        if (!user) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+
+        res.status(200).json(user);
+
+
+    } catch (err) {
+
+      console.error(err);
+
+      if (err.name === "CastError") {
+        return res.status(400).json({ message: "Invalid user ID" });
+      }
+
+      res.status(500).json({ message: "Error fetching user" });
+  }
+}
+
+
+
+const createUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword
+        });
+       
+
+        const savedUser = await newUser.save();
+        const userResponse = await User.findById(savedUser._id)
+        .select('-password');
+
+        res.status(201).json(userResponse);
+
+    } catch (err) {
+
+        if (err.name === "ValidationError") {
+            return res.status(400).json({
+                message: "Invalid user data",
+                details: err.errors
+            });
+        }
+
+        if (err.code === DUPLICATED_EMAIL_CODE) {
+            return res.status(409).json({
+                message: "Email already exists"
+            });
+        }
+
+        console.error(err);
+        return res.status(500).json({ message: "Error creating user" });
+    }
+}
+
+
+const updateUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const updateData = {};
+        if (name) updateData.name = name;
+        if (email) updateData.email = email;
+
+        if (password) {
+            updateData.password = await bcrypt.hash(password, 10);
+        }
+
+        const result = await User.findByIdAndUpdate(
+            req.params.id,
+            updateData,
+            {
+                new: true,
+                runValidators: true,
+                context: "query",
+            }
+
+        ).select('-password');
+
+
+        if (!result) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        res.status(200).json(result);
+
+    } catch (err) {
+
+        if (err.name === "ValidationError") {
+            return res.status(400).json({
+                message: "Invalid data",
+                details: err.errors
+            });
+        }
+
+        if (err.name === "CastError") {
+          return res.status(400).json({ message: "Invalid user ID" });
+        }
+    
+        if (err.code === DUPLICATED_EMAIL_CODE) {
+            return res.status(409).json({
+                message: "Email already exists"
+            });
+        }
+    
+        return res.status(500).json({ message: "Internal error" });
+    }
+}
+
+
+
+const deleteUser = async (req, res) => {
+    try {
+        const result = await User.findByIdAndDelete(req.params.id);
+
+        if (!result) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        res.status(200).send("User deleted successfully");
+
+    } catch (err) {
+
+
+        if (err.name === "CastError") {
+         return res.status(400).json({ message: "Invalid user ID" });
+        }
+        
+
+        console.error(err);
+        res.status(500).json({ message: "Error deleting user" });
+    }
+}
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+};
