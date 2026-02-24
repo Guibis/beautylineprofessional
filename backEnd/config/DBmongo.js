73abce8e2630ef402a89fbe6e23c9a1453a9c906
Igor Rodrigues Machado <107290125+igorrm19@users.noreply.github.com>
2026-02-24 12:28:28 -0300

    fix/resolve db connection issue (#64)
    * file DBmongp/corretion of error
    
    * resolve conflits

--- backEnd/config/DBmongo.js
+++ backEnd/config/DBmongo.js
@@ -3,20 +3,13 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
 
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-
-        });
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
 
     } catch (error) {
-        if (error.name === 'ValidationError') {
-            console.error(`Validation Error: ${error.message}`);
-            if (process.env.NODE_ENV !== 'test') process.exit(1);
-            throw error;
-        }
 
-        if (error.name === 'MongooseError' || (mongoose.Error && error instanceof mongoose.Error)) {
+        if (mongoose?.Error?.MongooseError && error instanceof mongoose.Error.MongooseError) {
             console.error(`Mongoose Error: ${error.message}`);
             if (process.env.NODE_ENV !== 'test') process.exit(1);
             throw error;
